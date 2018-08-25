25006aab4bb139f51fb017edb78d429a10d2bb62
Noah Medoff <nmedoff123@gmail.com>
2018-08-24 21:21:04 -0700

    create html for member/admin page

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
+import { PetitionService } from '../petition.service';
+import { Petition } from '../petition.model'
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component.css']
+  styleUrls: ['./admin.component.css'],
+  providers: [PetitionService]
 })
 export class AdminComponent {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
 
 }
